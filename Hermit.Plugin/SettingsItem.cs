0898f1967eb87da98d5efe25e11b8a672c0d900e
Lucas Brendel <lucasbrendel@gmail.com>
2014-12-28 08:51:38 -0600

    Project moved from Hermit.

---
+++ Hermit.Plugin/SettingsItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.ComponentModel.Composition;
+
+namespace Hermit.Plugin
+{
+    [Export(typeof(ISettingsItem))]    
+    public abstract class SettingsItem : PropertyChangedBase, ISettingsItem
+    {
+
+        public SettingsItem()
+        {
+            _DisplayName = "Settings Item";
+        }
+
+        private string _DisplayName;
+        public string DisplayName
+        {
+            get
+            {
+                return _DisplayName;
+            }
+            set
+            {
+                if(value != _DisplayName)
+                {
+                    _DisplayName = value;
+                    NotifyOfPropertyChange(() => DisplayName);
+                }
+            }
+        }
+    }
+}
