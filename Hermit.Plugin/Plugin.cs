7b5752524d33a9b907b4fb17b0c55f745672e9d1
Lucas Brendel <lucasbrendel@gmail.com>
2014-12-28 21:10:09 -0600

    Adding classes for the shell and for plugin

---
+++ Hermit.Plugin/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.ComponentModel.Composition;
+
+namespace Hermit.Plugin
+{
+    public class Plugin : PropertyChangedBase, IPlugin
+    {
+        private string _DisplayName;
+
+        public string DisplayName
+        {
+            get
+            {
+                return _DisplayName;
+            }
+            set
+            {
+                if(value != _DisplayName)
+                {
+                    _DisplayName = value;
+                    NotifyOfPropertyChange(() => DisplayName);
+                }
+            }
+        }
+
+        public Plugin()
+        {
+            DisplayName = "Plugin";
+        }
+    }
+}
---
+++ Hermit.Plugin/Shell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.ComponentModel.Composition;
+
+
+namespace Hermit.Plugin
+{
+    public class Shell : Screen, IShell
+    {
+        public Shell()
+        {
+            DisplayName = "Shell";
+        }
+    }
+}
