7308525e40c731a57dbbe75dba763cf2032f11e2
Lucas Brendel <lucasbrendel@gmail.com>
2014-12-28 20:46:20 -0600

    Added interface for startup tasks
    not tested yet.

---
+++ Hermit.Plugin/IHaveStartupTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermit.Plugin
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IHaveStartupTask
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void RunStartupTask();
+    }
+}
