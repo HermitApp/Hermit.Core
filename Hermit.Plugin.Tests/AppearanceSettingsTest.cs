8435b233b6e0e5267a1c82e97d5e4029e8814fcd
Lucas Brendel <lucasbrendel@gmail.com>
2014-12-31 10:49:08 -0600

    Adding unit test project.
    Need to test building and test executing on appveyor

---
+++ Hermit.Plugin.Tests/AppearanceSettingsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Hermit.Plugin;
+
+namespace Hermit.Plugin.Tests
+{
+    public class AppearanceSettingsTest
+    {
+        public class AccentColorDataTest
+        {
+            [Fact]
+            public void CreateObject()
+            {
+                AccentColorData accent = new AccentColorData();
+
+                accent.Name = "Red";
+
+                Assert.Equal("Red", accent.Name);
+            }
+        }
+    }
+}
